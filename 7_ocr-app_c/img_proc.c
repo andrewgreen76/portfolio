13bf50b038cea0b96bd126fc6e88c7a32bb88280
Andrew Green <andrewgreen031991@gmail.com>
2023-09-01 16:29:36 -0700

    x

--- 7_ocr-app_c/img_proc.c
+++ 7_ocr-app_c/img_proc.c
@@ -7,7 +7,9 @@ void get_img_props(ProcList * pm, BmpHeader * imgh54)
   FILE *srcbmp = fopen(pm->fname, "rb");
   fread(imgh54, sizeof(imgh54), 1, srcbmp);
 
-  printf("Signature: %c%c\n", (char)(imgh54->signature & 0xFF), (char)((imgh54->signature & 0xFF00) >>8) ); 
+  char loSigByte = (char)(imgh54->signature & 0xFF);
+  char hiSigByte = (char)((imgh54->signature & 0xFF00) >>8);
+  printf("Signature: %c%c\n", loSigByte, hiSigByte ); 
   printf("Total size of header: %d\n", imgh54->fileSize);
   printf("Reserved field check: %d\n", imgh54->reserved);
   
